18712e23c1c8f4962721698bc5c1c47a2897486c
samit-maersk <SamitKumar.Patel@maersk.com>
2023-06-14 23:55:35 +0200

    added 2 new verity project to test trace

---
+++ service-composition/src/main/java/com/example/servicecomposition/ServiceCompositionApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicecomposition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCompositionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCompositionApplication.class, args);
+	}
+
+}
---
+++ springboot-2-7-x/src/main/java/com/example/springboot27x/Springboot27XApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot27x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot27XApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot27XApplication.class, args);
+	}
+
+}
